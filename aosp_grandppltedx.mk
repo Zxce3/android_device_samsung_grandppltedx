53b3ccd80faf93b60c01458719dc58bab3039f2d
Ricky Cheung <rcheung844@gmail.com>
2020-11-23 20:10:42 +0800

    grandppltedx: AEX-ify
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- aosp_grandppltedx.mk
+++ aosp_grandppltedx.mk
@@ -1,8 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := grandppltedx
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/grandppltedx/device.mk)
@@ -16,9 +16,12 @@ DEVICE_RESOLUTION := 540x960
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
+# Bootanimation resoultion
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grandppltedx
-PRODUCT_NAME := lineage_grandppltedx
+PRODUCT_NAME := aosp_grandppltedx
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy J2 Prime
 PRODUCT_MANUFACTURER := Samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ export LC_ALL=C
 #export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
 
 # 8GB-dual
-add_lunch_combo lineage_grandppltedx-userdebug
-add_lunch_combo lineage_grandppltedx-eng
+add_lunch_combo aosp_grandppltedx-userdebug
+add_lunch_combo aosp_grandppltedx-eng
 
 # 16GB-single
